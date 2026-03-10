b3af5fad795348a72c2d307bf1f75700ea343110
nyaashlover <eduardogomesjr17@gmail.com>
2026-03-10 17:16:31 +0000

    Initialize variables and correct syntax errors
    Fix syntax errors and define variables in variables.php.

---
+++ variables.php
@@ -0,0 +1,26 @@
+<?php
+ 	//Single line comment
+
+
+
+
+
+
+ 	$output = 'Hello world'
+ 	$num1 = 4;
+ 	$num2 = 10;
+ 	$sum = $num1 +  $num2
+
+ 	$string 1 = 'Hello';
+ 	$string2 = 'World'
+ 	$greeting = $string1 . ' ' . $string2. '!';
+ 	$greeting2 = "$string1 $string2";
+
+ 	$string3 = "They\"re Here";
+
+ 	$float = 4.4;
+ 	$bool1 = true;
+
+ 	define('GREETING', 'Hello Everyone',)
+	echo greeting;
+?>
