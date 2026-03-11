92fa558624306df59564e4b87c69c3b8ac61be7f
nyaashlover <eduardogomesjr17@gmail.com>
2026-03-11 15:39:14 +0000

    Add common string functions documentation in PHP

---
+++ commonfunction.php
@@ -0,0 +1,75 @@
+<?php
+	# substr()
+	# Returns a portion of a string
+
+	//$output = substr('Hello', 1, 3);
+	//$output = substr('Hello', -2);
+	//echo $output;
+
+	# strlen()
+	# Returns the length of a string
+
+	//$output = strlen('Hello World');
+	//echo $output;
+
+	# strpos()
+	# Finds the position of the first occurence of a sub string
+
+	//$output = strpos('Hello World', 'o');
+	//echo $output;
+
+	# strrpos()
+	# Finds the position of the last occurence of a sub string
+
+	//$output = strrpos('Hello World', 'o');
+	//echo $output;
+
+	# trin()
+	# Strips whitespace
+
+	//$text = 'Hello world          ';
+	//var_dump($text);
+
+	//$trimmed = trim($text);
+	//var_dump($trimmed);
+
+	# strtoupper
+	# Makes everything uppercase
+
+	//$output = strtoupper('Hello world');
+	//echo $output
+
+	# strtolower
+	# Makes everything uppercase
+
+	//$output = strtolower('Hello world');
+	//echo $output
+
+	#ucwors«ds()
+	# Capitalize every word
+
+	//$output = ucwords('hello world');
+	//echo $output
+
+	# str_replace()
+	# Replace all occurence of a search string with a replacement
+
+	//$text= ('Hello world');
+	//$output = str_replace('world', 'Eduu', $text);
+	//echo $output
+
+	#is_string()
+	# Check if string
+
+	//$val = '22';
+	//$output = is_string($val);
+	//echo $output;
+
+	//$values = array(true, false, null, 'abc', 33, '33', 22.4, '22,4', '',' ', 8, '9');
+
+	//foreach($values as $value){
+		//if(is_string($value)){
+			//echo "($value) is a string<br>";
+		//}
+	//}
+?>
