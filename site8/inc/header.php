6a7a496d0b85298dbd2fa29327b6c060621aadf8
nyaashlover <eduardogomesjr17@gmail.com>
2026-03-13 12:50:15 +0000

    Add files via upload

---
+++ site8/inc/header.php
@@ -0,0 +1,10 @@
+<?php 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Blog</title>
+    <link rel="stylesheet" href="https://bootswatch.com/3/flatly/bootstrap.min.css">
+</head>
+<body>
+<?php include('navbar.php'); ?>
+<div class="container">
---
+++ site8/inc/navbar.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="<?php echo ROOT_URL; ?>">PHP Blog</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="<?php echo ROOT_URL; ?>">Home</a></li>
+    </ul>
+  </div>
+</nav>
+s
